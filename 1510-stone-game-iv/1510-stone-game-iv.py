3f47ce3491868022a19b0de0faa7b1313d5e8d36
Harshal Maurya <harshal.y.maurya@gmail.com>
2026-08-10 22:35:23 +0530

    Time: 853 ms (46.88%), Space: 15.7 MB (87.5%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def winnerSquareGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        dp = [False] * (n + 1)
+
+        for i in range(1, n + 1):
+            j = 1
+
+            while j * j <= i:
+                if dp[i - j * j] == False:
+                    dp[i] = True
+                    break
+
+                j += 1
+
+        return dp[n]
